fc8dc5cb97bfdff2a9aa9c21d4fd446b11fff75b
john <107359656+johnprakash-i@users.noreply.github.com>
2023-09-06 12:32:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center',
-            height: '250px', // Adjust the height as needed
+            height: '220px', // Adjust the height as needed
             marginBottom: '20px', // Adjust the height as needed
           }}
         />
